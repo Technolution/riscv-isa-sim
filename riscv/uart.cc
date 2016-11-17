4d7323669fdaa4a7640efafac9ff7c8e3c0923d5
Sijmen Woutersen <sijmen.woutersen@technolution.nl>
2016-11-17 15:45:03 +0100

    support for tl uart

--- riscv/uart.cc
+++ riscv/uart.cc
@@ -3,49 +3,49 @@
 #include "processor.h"
 #include <unistd.h>
 
-enum {
-  UART_DATA = 0,                  /* Data RX/TX */
-  UART_INTR_EN,                   /* Interrupt enable */
-  UART_INTR_ID,                   /* Interrupt identification */
-  UART_LINE_CTL,                  /* Line control */
-  UART_MODEM_CTL,                 /* Modem control */
-  UART_LINE_STAT,                 /* Line status */
-  UART_MODEM_STAT,                /* Modem status */
-  UART_SCRATCH                    /* Scratch register */
-};
-
-enum {
-  UART_LINE_STAT_THRE = (1 << 5), /* Transmitter holding register empty */
-  UART_LINE_STAT_TEMT = (1 << 6), /* Transmitter completely empty */
-};
-
 bool uart_t::load(reg_t addr, size_t len, uint8_t* bytes)
 {
-  /* For simplicity on our side, only support byte-sized accesses for now. */
-  if (len > 1)
+  uint32_t* word = (uint32_t*)bytes;
+  if (len != 4)
     return false;
 
   switch (addr)
   {
-    case UART_LINE_STAT:
-      *bytes = UART_LINE_STAT_TEMT | UART_LINE_STAT_THRE;
-      break;
+    case 0x14:  // tx status
+      *word = 2;    // empty
+      return true;
+    case 0x18:  // tx count
+      *word = 0;    // empty
+      return true;
+    case 0x1C:  // tx free
+      *word = 1024; // a lot
+      return true;
+    case 0x20:  // rx data
+      return false;
+    case 0x24:  // rx status
+      *word = 2;    // empty
+      return true;
+    case 0x28:  // rx count
+      *word = 0;    // empty
+      return true;
+    case 0x2C:  // rx free
+      *word = 1024; // a lot
+      return true;
     default:
-      *bytes = 0;
+      return false;
   }
-
-  return true;
 }
 
 bool uart_t::store(reg_t addr, size_t len, const uint8_t* bytes)
 {
-  if (len > 1)
+  uint32_t* word = (uint32_t*)bytes;
+  if (len != 4)
     return false;
 
   switch (addr)
   {
-    case UART_DATA:
-      putchar(*bytes);
+    case 0x10:  // tx data
+      putchar((*word) & 0xFF);
       fflush(stdout);
       break;
   }
